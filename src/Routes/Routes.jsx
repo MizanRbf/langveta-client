96adffb51a206d36e77c4dd582ee4941d98c3432
MizanRbf <mizanrbf@gmail.com>
2025-06-04 20:15:11 +0600

    pages file created and home page created inside that where there will be sadded some component

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,8 +1,15 @@
 import { createBrowserRouter } from "react-router";
+import RootLayout from "../Layouts/RootLayouts/RootLayout";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <h1>home</h1>,
+    Component: RootLayout,
+    children: [
+      {
+        index: true,
+        Component: <h1>HOme</h1>,
+      },
+    ],
   },
 ]);
