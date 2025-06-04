4c89adfac15f1bffdf6434da5ddad3ed39238cc7
MizanRbf <mizanrbf@gmail.com>
2025-06-04 20:09:59 +0600

    layouts file created and inside than a folder created names rootlayout whis is the main root where all project will be included

---
+++ src/Layouts/RootLayouts/RootLayout.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const RootLayout = () => {
+  return (
+    <div>
+      <header></header>
+      <main className="min-h-[calc(100vh-268px)]"></main>
+      <footer></footer>
+    </div>
+  );
+};
+
+export default RootLayout;
