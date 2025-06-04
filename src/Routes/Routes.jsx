e8921213bbffc5888174e67109de1cec4cafa2f9
MizanRbf <mizanrbf@gmail.com>
2025-06-04 22:55:09 +0600

    many routes created but not adding components still now

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,6 +3,7 @@ import RootLayout from "../Layouts/RootLayouts/RootLayout";
 import HomePage from "../Pages/Home/HomePage";
 import Login from "../Layouts/RootLayouts/AuthLayouts/Login";
 import Register from "../Layouts/RootLayouts/AuthLayouts/Register";
+import ErrorPage from "../Pages/Home/Error/ErrorPage";
 
 export const router = createBrowserRouter([
   {
@@ -38,5 +39,9 @@ export const router = createBrowserRouter([
         element: <h1>myBookedTutors</h1>,
       },
     ],
+  },
+  {
+    path: "/*",
+    Component: ErrorPage,
   },
 ]);
