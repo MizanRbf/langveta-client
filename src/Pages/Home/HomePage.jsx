016155f03df0a03e9647a5d7344e400d9d029585
MizanRbf <mizanrbf@gmail.com>
2025-06-06 11:13:06 +0600

    stats componet added in homepage where displays tutors,reviews,languages and users counts

--- src/Pages/Home/HomePage.jsx
+++ src/Pages/Home/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Banner from "../../Shared/Banner";
+import Stats from "./Stats";
 
 const HomePage = () => {
   return (
     <div className="">
       <Banner></Banner>
+      <Stats></Stats>
     </div>
   );
 };
---
+++ src/Pages/Home/Stats.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Stats = () => {
+  return (
+    <section className="p-6 dark:bg-gray-100 dark:text-gray-800">
+      <div className="container mx-auto grid justify-center grid-cols-2 text-center lg:grid-cols-4">
+        <div className="flex flex-col justify-start m-2 lg:m-6">
+          <p className="text-4xl font-bold leading-none lg:text-6xl">50+</p>
+          <p className="text-sm sm:text-base">Tutors</p>
+        </div>
+        <div className="flex flex-col justify-start m-2 lg:m-6">
+          <p className="text-4xl font-bold leading-none lg:text-6xl">89K</p>
+          <p className="text-sm sm:text-base">Reviews</p>
+        </div>
+        <div className="flex flex-col justify-start m-2 lg:m-6">
+          <p className="text-4xl font-bold leading-none lg:text-6xl">3</p>
+          <p className="text-sm sm:text-base">Languages</p>
+        </div>
+        <div className="flex flex-col justify-start m-2 lg:m-6">
+          <p className="text-4xl font-bold leading-none lg:text-6xl">8</p>
+          <p className="text-sm sm:text-base">Users</p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Stats;
