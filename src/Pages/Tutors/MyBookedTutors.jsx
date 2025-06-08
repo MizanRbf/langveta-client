490f0e6e9be6e7e807132b242a8863c3fd02fc46
MizanRbf <mizanrbf@gmail.com>
2025-06-08 08:05:00 +0600

    tutorBookings data inserted in mongodb by post method and after added successfully showed sweetalert

--- src/Pages/Tutors/MyBookedTutors.jsx
+++ src/Pages/Tutors/MyBookedTutors.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import { Link } from "react-router";
 
 const MyBookedTutors = () => {
   return (
     <div className="pt-25">
       <div className="max-w-[1400px] mx-auto px-4">
-        <h1>My Booked Tutors</h1>
+        <h1 className="pb-10">My Booked Tutors</h1>
+        <Link
+          className="bg-secondary text-white p-2 rounded-sm font-bold"
+          to={-1}
+        >
+          Go Back
+        </Link>
       </div>
     </div>
   );
--- src/Pages/Tutors/TutorDetails.jsx
+++ src/Pages/Tutors/TutorDetails.jsx
@@ -1,9 +1,37 @@
+import axios from "axios";
 import React from "react";
-import { useLoaderData } from "react-router";
+import { Link, useLoaderData } from "react-router";
+import useAuth from "../../Hooks/useAuth";
+import Swal from "sweetalert2";
 
 const TutorDetails = () => {
+  const { user } = useAuth();
   const tutor = useLoaderData();
   const { _id, name, image, language, price, review, description } = tutor;
+
+  // Handle Bookings
+  const handleBookings = () => {
+    const bookedTutors = {
+      tutorId: tutor._id,
+      image,
+      language,
+      price,
+      tutorEmail: tutor.email,
+      email: user.email,
+    };
+    axios
+      .post("http://localhost:3000/tutorBookings", bookedTutors)
+      .then((res) => {
+        if (res.data.insertedId) {
+          Swal.fire({
+            title: "Tutor Booked Successfully!",
+            icon: "success",
+            draggable: true,
+          });
+        }
+      });
+  };
+
   return (
     <div className="pt-25 px-4">
       <div className="max-w-[1400px] mx-auto px-4 border border-slate-200 rounded-sm">
@@ -26,9 +54,14 @@ const TutorDetails = () => {
           </div>
         </div>
       </div>
-      <button className="bg-secondary text-white py-6 text-2xl w-full rounded-b-sm font-bold cursor-pointer">
-        Book Now
-      </button>
+      <Link to="/myBookedTutors">
+        <button
+          onClick={handleBookings}
+          className="bg-secondary text-white py-6 text-2xl w-full rounded-b-sm font-bold cursor-pointer"
+        >
+          Book Now
+        </button>
+      </Link>
     </div>
   );
 };
