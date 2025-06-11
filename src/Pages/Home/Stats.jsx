1e6556b86b24f55e0201d1cd61b78da8a3e6336b
MizanRbf <mizanrbf@gmail.com>
2025-06-11 14:59:42 +0600

    react countup installed and applyed in stats section

--- src/Pages/Home/Stats.jsx
+++ src/Pages/Home/Stats.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import CountUp from "react-countup";
 
 const Stats = ({ allTutors, totalReviews, languageCount, totalUsers }) => {
   return (
@@ -6,25 +7,25 @@ const Stats = ({ allTutors, totalReviews, languageCount, totalUsers }) => {
       <div className="max-w-[1400px] mx-auto px-4 grid justify-center grid-cols-2 text-center lg:grid-cols-4">
         <div className="flex flex-col justify-start m-2 lg:m-6">
           <p className="text-4xl font-bold leading-none lg:text-6xl">
-            {allTutors.length}+
+            <CountUp end={allTutors.length} duration={20} />+
           </p>
           <p className="text-sm sm:text-base">Tutors</p>
         </div>
         <div className="flex flex-col justify-start m-2 lg:m-6">
           <p className="text-4xl font-bold leading-none lg:text-6xl">
-            {totalReviews}+
+            <CountUp end={totalReviews} duration={20} />+
           </p>
           <p className="text-sm sm:text-base">Reviews</p>
         </div>
         <div className="flex flex-col justify-start m-2 lg:m-6">
           <p className="text-4xl font-bold leading-none lg:text-6xl">
-            {languageCount}+
+            <CountUp end={languageCount} duration={20} />+
           </p>
           <p className="text-sm sm:text-base">Languages</p>
         </div>
         <div className="flex flex-col justify-start m-2 lg:m-6">
           <p className="text-4xl font-bold leading-none lg:text-6xl">
-            {totalUsers}+
+            <CountUp end={totalUsers} duration={20} />+
           </p>
           <p className="text-sm sm:text-base">Users</p>
         </div>
