deccdedbbe74ec81734a83cc74ce2b0001a0ecc2
MizanRbf <mizanrbf@gmail.com>
2025-06-11 15:15:45 +0600

    footer has been responsive

--- src/Shared/Footer.jsx
+++ src/Shared/Footer.jsx
@@ -1,21 +1,45 @@
 import React from "react";
-import { FaFacebook, FaLinkedin, FaXTwitter } from "react-icons/fa6";
+import { FaWhatsapp } from "react-icons/fa";
+import {
+  FaFacebook,
+  FaLinkedin,
+  FaLocationDot,
+  FaXTwitter,
+} from "react-icons/fa6";
+import { IoMail } from "react-icons/io5";
 import { Link } from "react-router";
 
 const Footer = () => {
   return (
-    <div className="bg-slate-800 text-white py-10">
-      <div className="max-w-[1800px] mx-auto px-4 flex justify-between">
-        {/* Company Logo */}
-        <div>
+    <footer className=" bg-[#131b00] text-white p-10 border border-x-0 border-t-primary border-b-0">
+      <div className="flex flex-col lg:flex-row *:mb-8 justify-between mb-10 max-w-[1800px] mx-auto md:px-4">
+        {/* Title & Description */}
+        <nav className="items-start *:space-y-2">
           <img
-            className="w-50 bg-white rounded-sm"
+            className="w-40 mb-8 bg-white rounded-sm"
             src="/assets/logo2.png"
-            alt=""
+            alt="Company Logo"
           />
-        </div>
+          {/* Contact Us */}
+          <nav className="*:text-left *:text-xs *:md:text-sm">
+            <p className="flex items-start gap-2">
+              <FaLocationDot />
+              16 sector,Cantonment,
+              <br /> Uttara-1209 ,Dhaka-1214
+            </p>
+            <p className="flex items-center gap-2">
+              <FaWhatsapp />
+              +880-1319444554
+            </p>
+            <p className="flex items-center gap-2">
+              <IoMail />
+              mailus@gmail.com
+            </p>
+          </nav>
+        </nav>
         {/* Quick Links */}
-        <div>
+
+        <nav className="flex flex-col *:text-sm space-y-2 text-left">
           <h4>Quick Links</h4>
           <nav className="flex flex-col *:hover:underline">
             <Link to="/">Home</Link>
@@ -24,37 +48,79 @@ const Footer = () => {
             <Link to="/myTutorials">My Tutorials</Link>
             <Link to="/myBookedTutors">My booked tutors</Link>
           </nav>
-        </div>
+        </nav>
         {/* Policies */}
-        <div>
-          <h4>Legal</h4>
-          <nav className="flex flex-col *:hover:underline">
-            <a href="">Terms & Conditions</a>
-            <a href="">License</a>
-            <a href="">Privacy Policy</a>
-            <a href="">All Right Reserved</a>
-            <a href=""></a>
-          </nav>
-        </div>
+        <nav className="flex flex-col *:text-sm *:space-y-2 text-left">
+          <ul className="*:hover:underline">
+            <h4 className="underline">Legal</h4>
+            <li>
+              <a href="">Terms & Conditions</a>
+            </li>
+            <li>
+              <a href="">License</a>
+            </li>
+            <li>
+              <a href="">Privacy Policy</a>
+            </li>
+            <li>
+              <a href="">All Right Reserved</a>
+            </li>
+            <li>
+              <a href=""></a>
+            </li>
+          </ul>
+        </nav>
         {/* Social Link */}
-        <div className="flex items-end">
+        <nav className="flex flex-col justify-end gap-6">
+          <h4 className="underline text-left">Visit Us</h4>
+          <ul className="flex text-3xl md:text-2xl w-full justify-start space-x-4">
+            <li>
+              <a
+                className="hover:text-primary"
+                href="https://www.facebook.com/"
+              >
+                <FaFacebook />
+              </a>
+            </li>
+            <li>
+              <a className="hover:text-primary" href="https://x.com/">
+                <FaXTwitter />
+              </a>
+            </li>
+            <li>
+              <a
+                className="hover:text-primary"
+                href="https://www.linkedin.com/"
+              >
+                <FaLinkedin />
+              </a>
+            </li>
+          </ul>
+
+          {/* form */}
           <div className="">
-            <h4>Social Links</h4>
-            <nav className="flex gap-2">
-              <a href="https://www.facebook.com/">
-                <FaFacebook size={20} />
-              </a>
-              <a href="https://x.com/">
-                <FaLinkedin size={20} />
-              </a>
-              <a href="https://www.linkedin.com/">
-                <FaXTwitter size={20} />
-              </a>
-            </nav>
+            <form className="w-full">
+              <div className="join">
+                <input
+                  type="email"
+                  placeholder="Email us"
+                  className="input input-bordered join-item"
+                />
+                <button className="btn btn-primary join-item">
+                  Send Email
+                </button>
+              </div>
+            </form>
           </div>
-        </div>
+        </nav>
       </div>
-    </div>
+      <div className="text-xs md:text-sm text-center">
+        <p>
+          Copyright © {new Date().getFullYear()} - All right reserved by
+          Langveta
+        </p>
+      </div>
+    </footer>
   );
 };
 
