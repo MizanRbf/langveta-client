68732439feec1873844755d4e7e2beccec352df9
MizanRbf <mizanrbf@gmail.com>
2025-08-10 19:12:30 +0600

    loading state updated with changing text

--- src/Shared/Loader.jsx
+++ src/Shared/Loader.jsx
@@ -4,7 +4,7 @@ const Loader = () => {
   return (
     <div className="flex justify-center items-center min-h-[calc(100vh-288px)]">
       <h2>
-        Data Loading <span className="loading loading-dots loading-xl"></span>
+        Loading <span className="loading loading-dots loading-xl"></span>
       </h2>
     </div>
   );
